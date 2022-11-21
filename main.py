f04228588643f839f920716ac59f1f491ffa9be5
Jacob, Wong Fung Kak <jacobwfk@gmail.com>
2022-11-21 03:22:31 +0000

    Update contents

--- main.py
+++ main.py
@@ -1,33 +1,13 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.get('/')
-def index():
-    return render_template('index.html')
+@app.get('/<page>')
+def get_page(page='index'):
+    return render_template(f'{page}.html')
 
-
-@app.get('/about')
-def about():
-    return render_template('about.html')
-
-
-@app.get('/components')
-def components():
-    return render_template('components.html')
-
-
-@app.get('/contact')
-def contact():
-    return render_template('contact.html')
-
-
-@app.get('/work')
-def work():
-    return render_template('work.html')
-
-
-@app.get('/works')
-def works():
-    return render_template('works.html')
+@app.post('/submit_form')
+def submit_form():
+    return redirect('/contact_success')
